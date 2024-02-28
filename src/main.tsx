0e7af457cb5e13c37584d6afd462e7fefe7d41e5
Cristhian Melo <rxtsel@hotmail.com>
2024-02-27 19:04:41 -0500

    feat: add react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import "@/styles/globals.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
-import "@/styles/globals.css";
+
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
