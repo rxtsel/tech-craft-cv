2ecdcc223573e51fd281cdffa32b16bb4e6b6620
Cristhian Melo <rxtsel@hotmail.com>
2024-03-02 17:25:05 -0500

    feat: add sign up page

---
+++ src/app/(views)/auth/signup/_components/login-form.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Button, FormField } from "@/components";
+import { LoginScheme, LoginFormData } from "@/schemes";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useForm } from "react-hook-form";
+
+export const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(LoginScheme)
+  });
+
+  const onSubmit = async (data: LoginFormData) => {
+    console.log("SUCCESS", data);
+  };
+
+  return (
+    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+      <FormField
+        type="email"
+        name="email"
+        placeholder="Email"
+        register={register}
+        error={errors.email}
+      />
+      <Button
+        disabled={isSubmitting}
+        type="submit"
+        className="w-full"
+        variant="secondary"
+      >
+        {isSubmitting ? (
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          "Sign In"
+        )}
+      </Button>
+    </form>
+  );
+};
--- src/app/(views)/auth/signup/page.tsx
+++ src/app/(views)/auth/signup/page.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { Button, Icons } from "@/components";
+import { LoginForm } from "./_components/login-form";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <section className="mx-auto flex w-full max-w-xs flex-col gap-4">
+      <header className="space-y-2 text-center">
+        <h1 className="text-2xl font-semibold ">Sign Up</h1>
+        <p className="text-muted-foreground">Create an account</p>
+      </header>
+
+      <div className="space-y-4">
+        <Button className="w-full">
+          <Icons.gh className="mr-2 h-4 w-4 fill-white" /> Register with GitHub
+        </Button>
+
+        <Button className="w-full" variant="outline">
+          <Icons.google className="mr-2 h-4 w-4" /> Register with Google
+        </Button>
+
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t"></span>
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+              or continue with
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <LoginForm />
+    </section>
+  );
 };
 
 export default SignUp;
