4c28af9ab3dd5c9984ba5dd23ca6a4d5b833b1ad
Cristhian Melo <rxtsel@hotmail.com>
2024-03-03 18:28:03 -0500

    chore: correct spell

--- src/lib/application/get-session.ts
+++ src/lib/application/get-session.ts
@@ -5,6 +5,6 @@ import { unstable_noStore as noStore } from "next/cache";
 
 export async function readUserSession() {
   noStore();
-  const supabsae = await createSupabaseServerClient();
-  return await supabsae.auth.getSession();
+  const supabase = await createSupabaseServerClient();
+  return await supabase.auth.getSession();
 }
