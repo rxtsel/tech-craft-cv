30574f881f3a07d33717a831ab07362027f3fb10
Cristhian Melo <rxtsel@hotmail.com>
2024-03-03 10:42:50 -0500

    chore: redirect to dashboard if user is logged

--- src/app/(views)/auth/layout.tsx
+++ src/app/(views)/auth/layout.tsx
@@ -1,10 +1,18 @@
 import { Footer } from "@/components";
+import { DASHBOARD_ROUTES } from "@/constants";
+import { readUserSession } from "@/lib/application";
+import { redirect } from "next/navigation";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
-const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+const AuthLayout: React.FC<AuthLayoutProps> = async ({ children }) => {
+  // redirect to dashboard if user is logged
+  const { data } = await readUserSession();
+
+  if (data.session) return redirect(DASHBOARD_ROUTES.dashboard);
+
   return (
     <>
       <main className="grid min-h-[calc(100dvh-28px)] place-items-center sm:min-h-[calc(100dvh-36px)]">
