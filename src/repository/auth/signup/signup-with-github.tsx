e7035bb46ef2cd6eb8955711d509766f94e5b357
Cristhian Melo <rxtsel@hotmail.com>
2024-03-03 12:41:44 -0500

    chore: add async await to gh button

--- src/repository/auth/signup/signup-with-github.tsx
+++ src/repository/auth/signup/signup-with-github.tsx
@@ -1,8 +1,8 @@
 import { AUTH_ROUTES } from "@/constants";
 import { supabase } from "@/lib/supabase/client";
 
-export const signUpWithGitHub = () => {
-  supabase.auth.signInWithOAuth({
+export const signUpWithGitHub = async () => {
+  await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
       redirectTo: `${window.location.origin}${AUTH_ROUTES.callback}`
